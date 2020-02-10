71f7b8491f097c054a2e2b239c4d2fa1d6720380
Richard Neher <richard.neher@unibas.ch>
2020-02-10 19:34:52 +0100

    added heat maps to seasonal analysis

--- fit_seasonal.py
+++ fit_seasonal.py
@@ -1,26 +1,40 @@
+"""
+script to run a simple seaonal SIR model with coupling to a global reservoir
+"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def dSIRdt(tSI, beta, eps, phi, rec, influx, turn_over):
-    infection = beta*(1+eps*np.cos(2*np.pi*(tSI[0]+phi)))*tSI[1]*tSI[2]
+def dSIRdt(tSI, beta, eps, phi, rec, influx, turnover):
+    '''
+    time derivative of a simple SIR model with an influx of infected
+    (e.g. holiday returnees) and population turnover. Seasonality is
+    modelled as harmonically oscillating infectivity with amplitude
+    eps and peak at phi
+    '''
+    infection = beta*(1+eps*np.cos(2*np.pi*(tSI[0]-phi)))*tSI[1]*tSI[2]
 
-    dS = - infection + (1-tSI[1])*turn_over - influx
-    dI = infection - (turn_over+rec)*tSI[2] + influx
+    dS = - infection + (1-tSI[1])*turnover - influx
+    dI = infection - (turnover+rec)*tSI[2] + influx
     return np.array([1, dS, dI])
 
+
 def run_SIR(X, tmax, dt):
-    turn_over = 0.03
+    '''
+    generate a trajectory for parameters X of length tmax with integration
+    time step dt
+    '''
+    beta, eps, phi, rec, influx, turnover = X
 
-    R0, eps, phi, rec, log10_influx = X
-    influx = 10**log10_influx
+    # initialize trajectory at steady state.
+    tSI = [np.array([0, rec/beta, turnover*(R0-1)/(R0*rec)])]
 
-    SI_vs_t = [np.array([0, 1/R0, 0.001])]
-    while SI_vs_t[-1][0]<tmax:
-        dSIR = dSIRdt(SI_vs_t[-1], R0*rec, eps, phi, rec, influx, turn_over)
-        SI_vs_t.append(SI_vs_t[-1] + dSIR*dt)
+    while tSI[-1][0]<tmax:
+        dSIR = dSIRdt(tSI[-1], beta, eps, phi, rec, influx, turnover)
+        tSI.append(tSI[-1] + dSIR*dt)
 
-    return np.array(SI_vs_t)
+    return np.array(tSI)
 
 def cost(X, prevalence, plot=False):
     dt = 0.001
@@ -36,18 +50,53 @@ def cost(X, prevalence, plot=False):
     return C
 
 if __name__ == '__main__':
-
+    # read average seasonal test-positive rate.
     df = pd.read_csv('frac_positive_by_month.tsv', sep='\t')
+    from itertools import product
+    # all time units are years. A rate of 1/day is hence 365/year
+    # 1/week is 52/year
+
     R0 = 3
     rec = 52
-    log10_influx = -3
+    influx = 1e-3
     eps = 0.5
-    X0 = (R0, eps, 1.0, rec, log10_influx)
+    turnover = 0.1 # rate at which people turn susceptible (0.1 corresponds to once every 10 years)
+    X0 = (R0, eps, 1.0, rec, influx, turnover)
 
     traj = run_SIR(X0, 30, 0.001)
-    cost(X0, df["all"])
+    cost(X0, df["all CoVs"])
     plt.plot(traj[3000:,0], traj[3000:,2])
     plt.ylim([0,0.003])
+
+    R0_vals = [1.5,2.5,4]
+    influx_vals =  [1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2]
+    eps_vals = np.linspace(0,0.8,9)
+    amplitudes = []
+    means = []
+    for R0, influx, eps in product(R0_vals, influx_vals, eps_vals):
+        X= (R0*rec, eps, 0.0, rec, influx, turnover)
+        traj = run_SIR(X, 30, 0.001)
+        num_points = len(traj)
+        print(R0, influx, eps, np.mean(traj[num_points//2:,2]))
+        # amplitudes.append(np.std(traj[num_points//2:,2])/np.mean(traj[num_points//2:,2]))
+        amplitudes.append(np.max(traj[3*num_points//4:,2])/np.min(traj[3*num_points//4:,2]))
+        means.append(np.mean(traj[num_points//2:,2]))
+
+
+    amplitudes = np.reshape(amplitudes, (len(R0_vals), len(influx_vals), len(eps_vals)))
+    means = np.reshape(means, (len(R0_vals), len(influx_vals), len(eps_vals)))
+
+    for ri, R0 in enumerate(R0_vals):
+        plt.figure()
+        plt.title(f'log10(max/min) of incidence, R0={R0}')
+        sns.heatmap(np.log10(amplitudes[ri]), xticklabels=[f"{x:1.1f}" for x in eps_vals],
+                    yticklabels=[f"{x:1.1e}" for x in influx_vals])
+        plt.tick_params('y', rotation=0)
+        plt.ylabel('import rate')
+        plt.xlabel('seasonality')
+        plt.tight_layout()
+        plt.savefig(f"figures/oscillations_{R0}.pdf")
+
 
     # from scipy.optimize import minimize
 
